0cec93a86bebc48d4107d6f985e0ebd588fcc212
Roman Dodin <dodin.roman@gmail.com>
2023-02-10 19:26:17 +0100

    added correct saved objects and removed index pattern script

--- create-index-pattern.sh
+++
@@ -1,6 +0,0 @@
-#/bin/sh
-# this script creates an index pattern for the index created by logstash
-# https://www.elastic.co/guide/en/kibana/7.17/index-patterns.html
-
-curl -X POST 'http://localhost:5601/api/saved_objects/index-pattern' -H 'kbn-xsrf: true' -H 'Content-Type: application/json' \
-  --data-raw '{"attributes":{"fieldAttrs":"{}","title":"fabric-logs-*","timeFieldName":"@timestamp","fields":"[]","typeMeta":"{}","runtimeFieldMap":"{}"}}'
