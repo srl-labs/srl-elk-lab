7275e06031160d731e168ccd6ee0a2d3be3c45b3
Roman Dodin <dodin.roman@gmail.com>
2023-02-10 13:29:00 +0100

    fixed index patterncreation

--- create-index-pattern.sh
+++ create-index-pattern.sh
@@ -1,11 +1,6 @@
 #/bin/sh
 # this script creates an index pattern for the index created by logstash
-# see https://www.elastic.co/guide/en/kibana/current/index-patterns-api-create.html
 # https://www.elastic.co/guide/en/kibana/7.17/index-patterns.html
-curl -X POST "localhost:5601/api/index_patterns/index_pattern" -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d'
-{
-  "index_pattern": {
-     "title": "fabric-logs-*"
-  }
-}
-'
+
+curl -X POST 'http://localhost:5601/api/saved_objects/index-pattern' -H 'kbn-xsrf: true' -H 'Content-Type: application/json' \
+  --data-raw '{"attributes":{"fieldAttrs":"{}","title":"fabric-logs-*","timeFieldName":"@timestamp","fields":"[]","typeMeta":"{}","runtimeFieldMap":"{}"}}'
