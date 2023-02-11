f60716747579d486f698c31e556171e0f83bd732
azyablov <anton0zyablov@gmail.com>
2023-02-11 12:52:57 +0100

    added pre-check of Kibana availability

--- add-saved-objects.sh
+++ add-saved-objects.sh
@@ -1,2 +1,7 @@
 # import saved searches and dashboards (overwrites existing objects with the same name)
-curl -X POST 'http://localhost:5601/api/saved_objects/_import?overwrite=true' -H "kbn-xsrf: true" --form file=@elk/kibana/saved-objects.ndjson
+nc -zv localhost 5601 > /dev/null 2>&1
+if [ $? == 0  ]; then
+    curl -X POST 'http://localhost:5601/api/saved_objects/_import?overwrite=true' -H "kbn-xsrf: true" --form file=@elk/kibana/saved-objects.ndjson
+else 
+    echo "Please wait a minute kibana isn't available yet (or maybe not started)."
+fi
